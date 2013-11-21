662cb6b3f8de20d40c24d3d25f1a83f29d315039
Andrew Marks <andrew.marks@navy.mil>
2013-11-21 08:43:29 -0800

    Rename Workspaces, show the name in the log
    Also, changed the way that xmobar works, hopefully more stable

--- xmonad.hs
+++ xmonad.hs
@@ -3,22 +3,28 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Actions.WorkspaceNames
+import XMonad.Prompt
 import System.IO
 
 main = do
-    xmproc <- spawnPipe "/usr/local/bin/xmobar"
+--    xmproc  <- spawnPipe "/usr/local/bin/xmobar"
+    spawn "/usr/local/bin/xmobar"
     xmonad $ defaultConfig
         { manageHook  = manageDocks <+> manageHook defaultConfig
         , layoutHook  = avoidStruts $ layoutHook defaultConfig
-        , logHook     = dynamicLogWithPP xmobarPP
-            { ppOutput = hPutStrLn xmproc
-            , ppTitle = xmobarColor "green" "" . shorten 50
-            }
+        , logHook     = workspaceNamesPP xmobarPP >>= dynamicLogString >>= xmonadPropLog
+--         , logHook     = dynamicLogWithPP xmobarPP
+--             { ppOutput = hPutStrLn xmproc
+--             , ppTitle = xmobarColor "green" "" . shorten 50
+--             } -- >>= dynamicLogString >>= xmonadPropLog
         , borderWidth = 1
         , terminal    = "urxvtc" } `additionalKeys`
-        [ ((mod4Mask    ,xK_l), spawn "slock") 
+        [ ((mod4Mask    ,xK_l), spawn "xlock") 
         , ((mod4Mask    ,xK_n), spawn "mpc next")
         , ((mod4Mask .|. shiftMask  ,xK_n), spawn "mpc prev")
         , ((mod4Mask    ,xK_j), spawn "mpc volume -7")
         , ((mod4Mask    ,xK_k), spawn "mpc volume +7")
+        , ((mod4Mask    ,xK_p), spawn "mpc toggle")
+        , ((mod4Mask    ,xK_r), renameWorkspace defaultXPConfig)
         ]
