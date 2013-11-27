9cae4b2aa75bf7f4945a1738aeb137f4d89389c5
Andrew Marks <andrew.marks@navy.mil>
2013-11-27 09:18:13 -0800

    Workspace names & some hooks

--- xmonad.hs
+++ xmonad.hs
@@ -7,17 +7,45 @@ import XMonad.Actions.WorkspaceNames
 import XMonad.Prompt
 import System.IO
 
+amWorkspaces = 
+    [ "1:Term"
+    , "2:Net"
+    , "3:Chat"
+    , "4:Doc1"
+    , "5:Doc2"
+    , "6:VM1"
+    , "7:VM2"
+    , "8:VM3"
+    , "9:EX1"
+    ]
+
+amManageHook = composeAll . concat $
+    [ [ className =? net   --> doShift "2:Net"  | net <- amNetShifts ]
+    , [ className =? chat  --> doShift "3:Chat" | chat <- amChatShifts ]
+    , [ ( className =? "Firefox" <&&> resource =? "Dialog") --> doFloat ]
+    , [ className =? fc    --> doFloat | fc <- amFloatClass ]
+    , [ manageDocks ]
+    ]
+    where
+        amNetShifts = 
+            [ "Firefox"
+            , "Thunderbird"
+            ]
+        amChatShifts = [ "Pidgin" ]
+        amFloatClass = 
+            [ "Wine"
+            , "MPlayer"
+            ]
+
+
+
 main = do
---    xmproc  <- spawnPipe "/usr/local/bin/xmobar"
     spawn "/usr/local/bin/xmobar"
     xmonad $ defaultConfig
-        { manageHook  = manageDocks <+> manageHook defaultConfig
+        { manageHook  = amManageHook
+        , workspaces  = amWorkspaces
         , layoutHook  = avoidStruts $ layoutHook defaultConfig
         , logHook     = workspaceNamesPP xmobarPP >>= dynamicLogString >>= xmonadPropLog
---         , logHook     = dynamicLogWithPP xmobarPP
---             { ppOutput = hPutStrLn xmproc
---             , ppTitle = xmobarColor "green" "" . shorten 50
---             } -- >>= dynamicLogString >>= xmonadPropLog
         , borderWidth = 1
         , terminal    = "urxvtc" } `additionalKeys`
         [ ((mod4Mask    ,xK_l), spawn "xlock") 
